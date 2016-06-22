cb87acc0bae1bca6b7738e462fc092492ba19a50
Vitor M. A. da Cruz <vitormazzi@gmail.com>
2016-06-22 15:39:25 -0300

    1.0, the is the first public version

--- setup.py
+++ setup.py
@@ -46,7 +46,7 @@ def get_readme():
 
 setup(
     name="python-fastpip",
-    version="0.0.5",
+    version="1.0",
     url='https://github.com/intelie/python-fastpip',
     author='Vitor M. A. da Cruz',
     author_email='vitor.mazzi@intelie.com.br',
